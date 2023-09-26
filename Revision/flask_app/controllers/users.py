28f5a13949ab05c746c9318f04ba3090a123f5c0
Dontsop-Anderson <kanadontsopanderson@gmail.com>
2023-09-26 18:27:07 +0100

    final commit (ui)

--- Revision/flask_app/controllers/users.py
+++ Revision/flask_app/controllers/users.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
@@ -23,12 +23,42 @@ def process():
         "dob": request.form['dob'],
         "password": hashed_pw
     }
-    User.save(data)
+    id = User.save(data)
+    session['user_id'] = id
     print(data)
     return redirect('/home')
 
+@app.route('/process-login', methods =['POST'])
+def login_user():
+    data = {
+        'email':request.form['email']
+    }
+    
+    user = User.get_user_by_email(data)
+    if not user:
+        flash("Invalid credentials", 'login')
+        return redirect('/login')
+    
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid credentials", 'login')
+        return redirect('/login')
+    
+    session['user_id'] = user.id
+    return redirect('/home')
+
+
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data = {
+        'id' : session['user_id']
+    }
+    return render_template('home.html', user = User.get_user_by_id(data))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
 
 
--- Revision/flask_app/models/user.py
+++ Revision/flask_app/models/user.py
@@ -1,5 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-
+from flask import flash
+import re
 
 class User:
     def __init__(self,data):
@@ -15,3 +16,41 @@ class User:
     def save(cls,data):
         query = "INSERT INTO users (name,email,dob,password,created_at,updated_at) VALUES (%(name)s,%(email)s,%(dob)s,%(password)s,NOW(),NOW());"
         return connectToMySQL('revise').query_db(query,data)
+    
+    @classmethod
+    def get_user_by_id(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('revise').query_db(query,data)
+        return cls(result[0])
+    
+    @classmethod
+    def get_user_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL('revise').query_db(query,data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+    
+    
+    @staticmethod
+    def validate_user(user):
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+        is_valid = True # we assume this is true
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL('revise').query_db(query, user)
+        if len(result) >= 1:
+            flash('Email already has an account', 'register')
+            is_valid = False
+        if len(user['name']) < 3:
+            flash("Name must be at least 3 characters.", 'register')
+            is_valid = False
+        if len(user['password']) < 8:
+            flash("Password must be at least 8 characters.", 'register')
+            is_valid = False
+        if user['password'] != user['confirm_pw']:
+            flash("Password doesn't match", 'register')
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address", 'register')
+            is_valid = False
+        return is_valid
